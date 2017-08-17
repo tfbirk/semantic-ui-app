ed8bec830a044d8c477a675dce2ba2e2389de05d
Taylor Brown <taylor.fra.brown@hpe.com>
2017-08-17 15:36:58 -0500

    1) make start visibility logic better 2) add Click Me behavior

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // script for index.html
-star_is_visible = false
 
 function action()
 {
     var star = document.getElementById("star")
+    var star_is_visible = star.style.display == "block"
     if (!star_is_visible)
     {
         star.style.display = "block"
@@ -15,4 +15,27 @@ function action()
         star_is_visible = false
     }
         
-} 
+}
+
+function changeBtnText()
+{
+    var click_me_btn = document.getElementById("click-me-btn")
+    var btn = document.getElementById("button")
+   
+    if (btn.innerHTML != "Click Me!")
+    {
+        btn.innerHTML = "Click Me!"
+        click_me_btn.innerHTML = "<i class=\"warning icon\"></i>"
+    }
+}
+
+function changeClickMeText()
+{
+    var click_me_btn = document.getElementById("click-me-btn")
+    var btn = document.getElementById("button")
+    if (click_me_btn.innerHTML != "Click Me")
+    {
+        click_me_btn.innerHTML = "Click Me"
+        btn.innerHTML = "<i class=\"warning icon\"></i>"
+    }
+}
